3a9962c37ad3a976bc84cf97a1c79730072e6836
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-03-11 11:15:27 +0700

    truyền props từ vào component từ state

---
+++ components/Box.jsx
@@ -0,0 +1,8 @@
+import react from "react";
+
+const Box = ({color}) => {
+    return(
+        <div style={{background: color, width: "40%", height: 100}}></div>
+    )
+}
+export default Box;
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Box from '../components/Box';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,11 +18,11 @@ function App() {
     <div>
         {show && <div>
             Number: {count} <br />
-            <div style={{background: color, width: "40%", height: 100}}></div> <br />
+            <Box color={color} /> <br />
             Boolean: {myStatus ? "Da ket hon" : "Chua ket hon"} <br />
             Arr: {products.map(item => <div key={item.id}>{item.name} <button onClick={() => removeItem(item.id)}>Delete</button></div>)}
         </div>}
-        <button onClick={() => setCount(count - 1)}>Desc count</button>    
+        <button onClick={() => setCount(prev => prev - 1)}>Desc count</button>    
         <button onClick={() => setCount(count + 1)}>Inc count</button>  <br/>
         <button onClick={() => setColor("red")}>Change Color</button>  <br/>
         <button onClick={() => setShow(!show)}>Hidden</button>    
