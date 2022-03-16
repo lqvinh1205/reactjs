2c9df4c7964f39cce01c716e9d59ed4af7ffb1b8
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-03-16 11:12:50 +0700

    sử dụng useParams() lấy params từ url

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,17 @@ import ProductsPage from './pages/ProductsPage'
 import AdminLayout from './pages/layouts/AdminLayout'
 import DashboardPages from './pages/DashboardPages'
 import ProductsManager from './pages/ProductsManager'
+import ProductDetails from './pages/ProductDetails'
 function App() {
   const [products, setProducts] = useState<{_id: number, name: string}[]>([])
   useEffect(() => {
     const getListProducts = async () => {
-      const {data} = await axios.get("https://61e6b529ce3a2d0017359384.mockapi.io/users")
+      const {data} = await axios.get("http://localhost:3001/api/products")
       console.log(data);
-      
       setProducts(data)
     }
-  }) 
-  console.log(products);
+    getListProducts()
+  },[]) 
   
   return (
     <div className="App">
@@ -32,19 +32,22 @@ function App() {
           <li><NavLink to="/">Home page</NavLink></li>
           <li><NavLink to="/products">Product</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/admin">Admin</NavLink></li>
+          <li><NavLink to="/admin/products">Dashboard</NavLink></li>
         </ul>
       </header>
       <main>
         <Routes>
           <Route path='/' element={<WebsiteLayout />}>
               <Route index element={<HomePages />} />
-              <Route path='products' element={<ProductsPage />} />
+              <Route path='products' element={<ProductsPage />}>
+              </Route>
           </Route>
           <Route path='admin' element={<AdminLayout />}>
                 <Route index element={<Navigate to="dashboard" />} />
                 <Route path='dashboard' element={<DashboardPages />} />
                 <Route path='products' element={<ProductsManager />} />
-
+                <Route path='product/:id' element={<ProductDetails />} />
           </Route>
         </Routes>
       </main>
