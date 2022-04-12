21c361188dd9dd75b9638a1e9fd77be58d9e0966
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-04-12 09:49:45 +0700

    .

--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -1,21 +1,16 @@
 import { Button, notification } from "antd";
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { getLs, removeLs } from "../ultis/localstogare";
+import { signOut } from "../features/auth/authSlice";
+import { getLs } from "../ultis/localstogare";
 import "./HeaderTop.css";
 
 type Props = {};
 
 const HeaderTop = (props: Props) => {
-    const isSignin = getLs("user")
-    const navigate = useNavigate()
-    const signOut = () => {
-        removeLs('user')
-        navigate("/")
-        notification.success({
-          message: "Đăng xuất thành công"
-        })
-    }
+  const isSignin = getLs("user");
+  const dispatch = useDispatch();
 
   return (
     <div className="header-top container-main">
@@ -32,17 +27,17 @@ const HeaderTop = (props: Props) => {
         <ul>
           <li>
             {/* <a href="#pro-new">Home</a> */}
-            <Link to='/'>Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to='/products'>Shop</Link>
+            <Link to="/products">Shop</Link>
           </li>
           <li>
             <a href="#carousel-cars">Populor</a>
           </li>
           <li>
             <a href="#more-future">About</a>
-          </li>      
+          </li>
           <li>
             <a href="#featured-lux">Featured</a>
           </li>
@@ -51,7 +46,7 @@ const HeaderTop = (props: Props) => {
       <div className="btn-login">
         {isSignin ? (
           <Link to="/">
-            <Button type="dashed" ghost onClick={signOut}>
+            <Button type="dashed" ghost onClick={() => dispatch(signOut())}>
               Log-out
             </Button>
           </Link>
--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,8 +1,9 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { notification } from "antd";
 import { AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
 import { signin } from "../../api/auth";
-import { setLs } from "../../ultis/localstogare";
+import { removeLs, setLs } from "../../ultis/localstogare";
 
 export const signIn = createAsyncThunk("auth/signIn", async (user: any) => {
   try {
@@ -12,28 +13,47 @@ export const signIn = createAsyncThunk("auth/signIn", async (user: any) => {
       message: "Thông báo",
       description: "Đăng nhập thành công!",
     });
-  } catch(error) {
-    const err = error as AxiosError
-        if (err.response) {
-           notification.error({
-            message: err.response.data.messages,
-          });
-        }
+  } catch (error) {
+    const err = error as AxiosError;
+    if (err.response) {
+      notification.error({
+        message: err.response.data.messages,
+      });
+    }
   }
 });
-
+export const signOut = createAsyncThunk("auth/signIn", async () => {
+  try {
+    const navigate = useNavigate();
+    removeLs("user");
+    navigate("/");
+    notification.success({
+      message: "Đăng xuất thành công",
+    });
+  } catch (error) {
+    const err = error as AxiosError;
+    if (err.response) {
+      notification.error({
+        message: err.response.data.messages,
+      });
+    }
+  }
+});
 const authSlice = createSlice({
   name: "auth",
   initialState: {
     auth: {},
-    isSignin: false
+    isSignin: false,
   },
-  reducers: {
-  },
+  reducers: {},
   extraReducers: (builder: any) => {
     builder.addCase(signIn.fulfilled, (state: any, action: any) => {
       state.auth = action.payload;
       state.isSignin = true;
+    });
+    builder.addCase(signOut.fulfilled, (state: any, action: any) => {
+      state.auth = action.payload;
+      state.isSignin = false;
     });
   },
 });
